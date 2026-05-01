20a40eb82ef64901380601fb81a7e23ed69b7400
Sisay Shimelash <sisayshimelash@gmail.com>
2026-05-01 12:00:51 +0300

    app.jsx error fixed

--- react-apple/src/main.jsx
+++ react-apple/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // 👈 Add this import
+import { BrowserRouter } from "react-router-dom"; 
 import "./index.css";
 import App from "./App.jsx";
 
